7e8c2cb374cb06ef8de4432f36160ec34e3e3135
Antonio Jesús Ramírez Arcos <antoniojra99@gmail.com>
2025-05-12 18:44:38 +0200

    Delete Frontend/src/app/utils directory

--- Frontend/src/app/utils/translate-loader.ts
+++
@@ -1,7 +0,0 @@
-// src/app/utils/translate-loader.ts
-import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
